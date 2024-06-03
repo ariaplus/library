e0dc78ae01f191f2e6bbc417865c59f6f7a6da51
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-06-04 04:27:03 +1200

    Create verified-badges.tsx

---
+++ verified-badges.tsx
@@ -0,0 +1,32 @@
+
+
+
+verifiedgovernment: boolean;
+verifiedgovernmentdepartments: boolean;
+verifiedplus: boolean;
+verifiedrepublican: boolean;
+verifieddemocrat: boolean;
+verifiedsmallbusiness: boolean;
+verifiedorganisation: boolean;
+verifiedcorporation: boolean;
+verifiedlandmark: boolean;
+verifiedmembersofcongress: boolean;  
+verifiedcontentcreator: boolean;
+verifiedcompany: boolean;
+verifiedpresidents: boolean;
+verifiedpresidentialcandidates: boolean;
+verifiedpage: boolean;
+verifiedoracle: boolean;
+verifiedjournalist: boolean;
+verifiedwarjournalist: boolean;
+verifiedinfluencer: boolean;
+verifiedNBA: boolean;
+verifiedNFL: boolean;
+verifiedMLB: boolean;
+verifiedMLS: boolean;
+verifiedNASCAR: boolean;
+verifiedPremierLeague: boolean;
+verifiedNGO: boolean;
+verifiedWanda: boolean;
+verifiedBeta: boolean;
+
