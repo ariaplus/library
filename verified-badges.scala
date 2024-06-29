12cf2556f8127021de6cbef4dbf133493e3334ff
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-06-30 11:55:53 +1200

    Update and rename verified-badges.tsx to verified-badges.scala

---
+++ verified-badges.scala
@@ -0,0 +1,52 @@
+object AriaplusVerificationBadges {
+  def main(args: Array[String]): Unit = {
+    val text = "ariaplus-verification-tags"
+    println(text)
+  }
+}
+
+
+verifiedGovernment: boolean;
+verifiedGovernmentDepartments: boolean;
+verifiedPlus: boolean;
+verifiedRepublican: boolean;
+verifiedDemocrat: boolean;
+verifiedSmallBusiness: boolean;
+verifiedOrganisation: boolean;
+verifiedCorporation: boolean;
+verifiedLandmark: boolean;
+verifiedMembersofCongress: boolean;  
+verifiedContentCreator: boolean;
+verifiedCompany: boolean;
+verifiedPresidents: boolean;
+verifiedPresidentialCandidates: boolean;
+verifiedPage: boolean;
+verifiedOracle: boolean;
+verifiedJournalist: boolean;
+verifiedWarJournalist: boolean;
+verifiedInfluencer: boolean;
+verifiedNBA: boolean;
+verifiedNFL: boolean;
+verifiedMLB: boolean;
+verifiedMLS: boolean;
+verifiedNASCAR: boolean;
+verifiedPremierLeague: boolean;
+verifiedNGO: boolean;
+verifiedWanda: boolean;
+verifiedBeta: boolean;
+verifiedArtist: boolean;
+verifiedAthlete: boolean;
+verifiedCollege: boolean;
+verifiedSchool: boolean;
+verifiedInstitution: boolean;
+verifiedCommunity: boolean;
+verifiedEntrepreneur: boolean;
+verifiedNonprofit: boolean;
+verifiedVideoGame: boolean;
+verifiedPet: boolean;
+verifiedSociety: boolean;
+verifiedModel: boolean;
+verifiedShop: boolean;
+verifiedAI: boolean;
+
+
