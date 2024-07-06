d5f323018fec6696ba2992a7268254ce9f2e476e
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-07-06 12:03:20 +1200

    Create product-checks.scala

---
+++ product-checks.scala
@@ -0,0 +1,24 @@
+object AriaplusVerificationBadges {
+  def main(args: Array[String]): Unit = {
+    val text = "ariaplus-verification-tags"
+    println(text)
+  }
+}
+
+
+isBook
+isClothing
+isTech
+isFood
+isUsed
+isNo1Bestseller
+isTopGrossing
+isGiftCard
+
+isForSchoool
+isForWomen 
+isForMen
+isForKids
+isForLGBTQIA
+isForAnimals
+isForPets
