cab649160fc8eb7dc92bae2823d18e9cd8f99f0a
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-07-08 10:07:04 +1200

    Create temp-cat.scala

---
+++ market/data/temp-cat.scala
@@ -0,0 +1,384 @@
+object Home Furniture and Appliances - {
+Shop All 
+Furniture
+Mattress and Accessories 
+Kitchen & Dining
+Storage & Organiastion 
+Luggage & Travel 
+Appliances
+Kitchen Apppliacnes 
+Decor
+Bedding
+Bath
+Deals
+}
+
+object Patio and Garden - {
+  Shop All
+  Patio Furniture
+  Patio Sets 
+  Conversation Sets 
+  Outdoor Cooking
+  Gas Grills
+  Charcoal
+  Outdoor Decor 
+  Outdoor Rugs 
+  Outdoor Cushions 
+  Outdoor Power Equipment
+  Lawn Mowers 
+  Lawn Care 
+  Bird Seed
+  Pool Chemicals 
+  Gardebn Center
+  Soil 
+  Mulch
+  Live Plants
+  Deals
+}
+
+
+object Home Improvement - {
+  shop All 
+  Heaters
+  humidifiers 
+  air quality
+  water purification 
+  tools
+  fastners 
+  buiilding materials 
+  fans 
+  wallpaper and wall coverings
+  kitchen renovation
+  batheoom renovation
+  shower heads
+  toilet seats and lids 
+  paint
+  flooring
+  light bulbs 
+  light fixtures 
+  garage storage 
+  personal protective equipment 
+  wallpaper and wall coverings 
+  adhesives and glues
+  dehumidifiers 
+  air conditioners 
+  deals,
+}
+
+object Clothing Shoes and Accesories - {
+  shop all
+  new arrivals
+  editors picks 
+  now trending
+  womens spring trends
+  women
+  womens plus 
+  men 
+  young adults 
+  kids 
+  baby and toddlers 
+  shoes 
+  jewelery and watches 
+  bags and accessories 
+  savings , 
+}
+
+object Toys - {
+  shop all 
+  action figures 
+  dolls and dollhouses
+  learning toys 
+  arts and crafts 
+  games and puzzels 
+  collectives 
+  cars, drones and RC
+  LEGO and building sets 
+  pretend play
+  outdoor toys 
+  kids bikes
+  ride on toys 
+  savings 
+}
+
+object Baby - {
+  Shop All
+  nursury
+  car seats 
+  strollers 
+  gear and activity
+  diapers and wipes
+  feeding and nursing 
+  baby and toddler toys
+  bath and potty
+  baby health and safety
+  baby clothing
+  toddler clothing 
+  toddlers room 
+}
+
+object Video Games - {
+  Shop All
+  Xbox 
+  Playstation
+  nintendo
+  video game accessorie
+  virtual reality
+  savings 
+
+}
+
+object Electronics - {
+  Shop All 
+  tv and video
+  home and audio theater 
+  smart home
+  tablets and accessories 
+  computers
+  cell phones 
+  wearable tech
+  camera, camcorders and drones 
+  photo services
+  portable audio
+  auto electronics 
+  video games
+  savings , 
+}
+
+object Pharmacy Health and Wellness - {
+  shopp all health
+  shop all wellness
+  pharmacy 
+  covid-19 testing kits 
+  vitamins and supplements
+  pain relievers
+  allergy, sinus and asthama
+  cough, cold and flu 
+  weight managment 
+  protein and performance nutrition 
+  shop all wellness
+  diabetes management 
+  medicine cabinet 
+  home health care
+  digestive health 
+  womens health 
+  deals
+}
+
+object Beauty - {
+  shop all
+  viral beauty picks
+  new arrivals
+  suncare and tanning 
+  beauty box
+  walmart start
+  beauty space NK
+  curly hair and more store 
+  shop all premium beauty
+  makeup
+  skincare 
+  hair care
+  fragrance
+  nail care 
+  beauty tech and tools
+  bath and body 
+  mens grooming
+  deals
+}
+
+object Personal Care - {
+  shop all 
+  multipacks
+  bath and body
+  oral care
+  feminine care 
+  incontinence
+  sexual wellness 
+  shaving 
+  mens grooming
+  sunscreen
+  self tanners
+  deals
+}
+
+object Auto and Tires - {
+  shop all 
+  tires
+  batteries and accessories
+  auto care center
+  oils and fluids
+  replacement auto parts 
+  auto electronics
+  exterior car parts and assessories
+  interior accessories 
+  automotive tools and equipment 
+  auto detailing and care center 
+  wheels and rims 
+  truck parts and accessories 
+  RV parts and Accesories
+  oem parts 
+  motorcycle parts and accessories 
+  vehicles
+  automotive apparel
+  car safety and car security 
+  deals
+}
+
+object Household Esentials - {
+  shop all 
+  food plan essentials
+  as seen on tv
+  pest control 
+  laundry room 
+  kitchen
+  bathroom 
+  cleaning supplies 
+  paper and plastic 
+  air freshners
+  batteries
+  lugh bulbs
+  the mindful living shop
+  deals 
+}
+
+object Sports and outdoors - {
+  shop all
+  sports 
+  basketball
+  football
+  golf 
+  pickleball
+  soccer
+  fitness
+  outdoor sports 
+  bikes
+  boats and marine
+  camping 
+  fishing 
+  hunting
+  sports shooting
+  outdoor toys 
+  trampolines
+  swing sets
+  swimming pools and spas
+  deals
+}
+
+object Pets - {
+  shop all 
+  dog supplies
+  cat supplies
+  fish supplies
+  small snimal 
+  reptile supplies
+  bird supplies
+  horse supplies
+  farm anmal supplies
+  deals
+}
+
+object School and Office Supplies - {
+  shop all 
+  school suppliers
+  teachers classrooom shop
+  bulk teaching supplies
+  school arts and crafts 
+  shipping and moving
+  safes and lockboxes 
+  walmart for business
+  pens 
+  notebooks and pads
+  calenders and planners 
+  markers and highlighters 
+  paper
+  pencils adn pencil sharpners
+  deals
+}
+
+object Arts Crafts Sewing and Party Supplies - {
+  shop all 
+  sewing machines and accessories
+  die cutting 
+  fabric 
+  arts and crafts furniture and storage
+  craft supplies
+  art supplies
+  knitting and crotchet 
+  arts and crafts for kids 
+  beading and jewelery making
+  fabric and apparel crafting
+  artificial plants and flowers
+  party supplies 
+  gift wrap 
+  deals,
+}
+
+object Movies and tv - { 
+  shop all 
+  4k Ultra HD Movies 
+  Blu-ray Movies 
+  DVD Movies
+  4K Ultra HD TV Shows 
+  Blu-ray TV Shows 
+  DVD TV Shows 
+  savings , 
+}
+
+object Music and Vinyl - {
+  shop all
+  turntables
+  musical intruments 
+  rap and hip hop
+  rock 
+  pop
+  country
+  classical 
+  savings 
+}
+
+object Books - {
+  shop all books
+  kobo eReaders 
+  ebook
+  childrens books
+  textbooks 
+  teen and young adult books 
+  magazines
+  biographers adn memoirs 
+  literature and fiction 
+  cosmic adn graphic novels 
+  arts and entertainment 
+  cookbooks, food and wine 
+  savings ,
+}
+
+
+object Seasonal Celebrations - {
+  shop all
+  baby showers
+  balloons
+  balloon arches
+  balloons by color
+  birthday party supplies
+  gender reveal
+  helium tanks
+  themed party supplies
+}
+
+object Gift Cards - {
+  gift cards
+  gaming gift cards
+  returaunt gift cards
+  retail gift crads
+  prepaid gift cards
+  entertainment gift cards
+  travel gift cards
+  prepaid minutes gift cards
+  lifestyle gift cards
+  plastic gift cards 
+  egift cards
+}
+
+
+
+  
+  
