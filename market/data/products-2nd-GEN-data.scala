7c71d8dfbd93957c1aa7eedde280fb351a779cd4
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-07-12 14:24:59 +1200

    Update products-2nd-GEN-data.scala

--- market/data/products-2nd-GEN-data.scala
+++ market/data/products-2nd-GEN-data.scala
@@ -171,6 +171,236 @@ producthasAriaplusProtectionPlan
 
 
 
+/ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+productId,text,images,parent,productCreatedBy,productCreatedAt,productUpdatedAt,productShippingInfo,productDeliveryInfo,productReturnInfo,productShopInfo,productBrandInfo,productIsVerified,productIsCosmetic,productIsFood,
+productIsForWomen,productIsForMen,productIsForKids,productIsEndorsed,productIsBoosted,productIsOnSale,productBusinessLocation,productIngredients,productInstructions,productIMG02,productIMG03,productIMG04,productIMG05,
+productWebsite,productPrice,productIsABook,productIsTopRated,productHasMoreCollections,viewProductRef,productAriaplusMarketIsNotAvailableInUsersLocationBUYBOX,productItemCannotBeShippedToUsersLocationBUYBOX,
+productStandardBUYBOX,productIsCurrentlyUnavailableBUYBOX,productStandardBuyBox,productHasNutritionBox,productColorIsBlue,productColorIsRed,productColorIsGreen,productColorIsYellow,productColorIsBlack,productColorIsWhite,
+productColorIsPurple,productColorIsOrange,productColorIsPink,productHasSizeXS,productHasSizeS,productHasSizeM,productHasSizeMS,productHasSizeL,productHasSizeXL,// PRODUCT DETAILS // 
+productColor,productSize,productSubscription,productConfiguration,productWeWantYouToKnow,productBrand,productDimensions,productMaterials,productConnectorType,productAboutThisItem,productMetalType,productClaspType
+productChainType,productGenType,productItemType,productItemDisplayLenght,productItemWidth,productRecommendedUses,productOccassion ,productCapacity,productItemWight,productASIN,productItemModelNumber,productBestSellersRank
+productDateFirstAvailable,productManufacturer,productWhatsInTheBox,ProductDescription,productConectivityTechnology,productPrintingTechnology,productSpecialFeature,productModelName,productPrinterOutput,productMaximumPrintSpeed,productCompatibleMountings
+productAspectRatio,productPhotoSensorTechnology,productSupportedFileFormat,productImageStabalization,productMaximumFocalLenght,productOpticalZoom,productMaximumAperture,productMaximumExpandedISO,productDeliveryandSupport
+productInkColor,productPageYeild,productIncludedComponents,productScreenSize,productResolution,productScreenSurfaceDescription,productFrequencyBandClass,productWirelessCommunicationStandard,productFrequency,productOperatingSystem
+productCPUModel,productCPUSpeed,productGraphicsCardDescription,productMemoryStorageCapacity,productMemorySlotsAvailable,productRamMemoryInstalledSize,productMountingType,productUPC,productOperatingMode,productCurrentRating
+productOperatingVoltage,productTerminal,productSwitchType,productSpecificationMet,productCircuitType,productActuatorType,productCondition,productBatteryLife,productHeight,productWeight,productWidth,productDepth,productMountings,productChip
+productVideo,productDisplay,productRatings,productCompatibility,productBatteries,productEarPlacement,productFormFactor,productNoiseControl,productHardDiskSize,productGPU,productNetworking,productInTheBox,productDesignName,productShape,productOS
+productFabricType,productCareInstructions,productRingSize,productGenTYpe,productEditon,productPltaform,productSurroundSound,productAudio,productServiingSize,productCalories,productTotalFat,productStauratedFat,productTransFat,productColestrol
+productSodium,productProtein,productVitaminA,productVitaminB,productVitaminC,productVitaminD,productCalcium,productIron,productPotassium,productIngredients,productUnitCount,productProduceSoldAs,productVariety,productNumberofItems
+productGlutenFree,productFreshnessGuaranteed,productDietType,productUSDAOrganic,productKosher,productDietaryFibre,productSugars,productFlavourName,productSpecialty,productAllergenInfo,productAgeRange,productSpecificUsesForItem
+productSpecialIngredients,productServingsPerContainer,productTypicalPrice,productListPrice,productCuisine,productJoinProLabelonPage,productBiologicalSource,productTempretureSource,productYouSaveLabel,productPickupFrom,productCategory,
+// NEW PARAMERTERS // 
+produuctisUnavailable,productCannotBeShippedToUser,productGetUpto100ToYourAccountLabel
+productisAriaplusTopChoice,productRatings,productSave20PercentWithTradeIn,productIsTheLatestModel,productWorksWithAi,productFreeInternationalShipping,productFreeUSShipping,productisAvailableAtALowerPriceFromOtherSellers
+productThereIsAnotherVersionOfThisProduct,productPurchasesThisMonth,productWarrantyandSupport,productNote,product1YearWarrantyand30DayReturnsPolicy,productCustomersUsuallyKeepThisItem,productLimitedTimeOffer,producthasAriaplusProtectionPlan,
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+productId , text , images , parent , productCreatedBy , productCreatedAt , productUpdatedAt , productShippingInfo , productDeliveryInfo , productReturnInfo , productShopInfo , productBrandInfo , productIsVerified , productIsCosmetic , productIsFood , 
+productIsForWomen , productIsForMen , productIsForKids , productIsEndorsed , productIsBoosted , productIsOnSale , productBusinessLocation , productIngredients , productInstructions , productIMG02 , productIMG03 , productIMG04 , productIMG05 , 
+productWebsite , productPrice , productIsABook , productIsTopRated , productHasMoreCollections , viewProductRef , productAriaplusMarketIsNotAvailableInUsersLocationBUYBOX , productItemCannotBeShippedToUsersLocationBUYBOX , 
+productStandardBUYBOX , productIsCurrentlyUnavailableBUYBOX , productStandardBuyBox , productHasNutritionBox , productColorIsBlue , productColorIsRed , productColorIsGreen , productColorIsYellow , productColorIsBlack , productColorIsWhite , 
+productColorIsPurple , productColorIsOrange , productColorIsPink , productHasSizeXS , productHasSizeS , productHasSizeM , productHasSizeMS , productHasSizeL , productHasSizeXL , // PRODUCT DETAILS // 
+productColor , productSize , productSubscription , productConfiguration , productWeWantYouToKnow , productBrand , productDimensions , productMaterials , productConnectorType , productAboutThisItem , productMetalType , productClaspType
+productChainType , productGenType , productItemType , productItemDisplayLenght , productItemWidth , productRecommendedUses , productOccassion  , productCapacity , productItemWight , productASIN , productItemModelNumber , productBestSellersRank
+productDateFirstAvailable , productManufacturer , productWhatsInTheBox , ProductDescription , productConectivityTechnology , productPrintingTechnology , productSpecialFeature , productModelName , productPrinterOutput , productMaximumPrintSpeed , productCompatibleMountings
+productAspectRatio , productPhotoSensorTechnology , productSupportedFileFormat , productImageStabalization , productMaximumFocalLenght , productOpticalZoom , productMaximumAperture , productMaximumExpandedISO , productDeliveryandSupport
+productInkColor , productPageYeild , productIncludedComponents , productScreenSize , productResolution , productScreenSurfaceDescription , productFrequencyBandClass , productWirelessCommunicationStandard , productFrequency , productOperatingSystem
+productCPUModel , productCPUSpeed , productGraphicsCardDescription , productMemoryStorageCapacity , productMemorySlotsAvailable , productRamMemoryInstalledSize , productMountingType , productUPC , productOperatingMode , productCurrentRating
+productOperatingVoltage , productTerminal , productSwitchType , productSpecificationMet , productCircuitType , productActuatorType , productCondition , productBatteryLife , productHeight , productWeight , productWidth , productDepth , productMountings , productChip
+productVideo , productDisplay , productRatings , productCompatibility , productBatteries , productEarPlacement , productFormFactor , productNoiseControl , productHardDiskSize , productGPU , productNetworking , productInTheBox , productDesignName , productShape , productOS
+productFabricType , productCareInstructions , productRingSize , productGenTYpe , productEditon , productPltaform , productSurroundSound , productAudio , productServiingSize , productCalories , productTotalFat , productStauratedFat , productTransFat , productColestrol
+productSodium , productProtein , productVitaminA , productVitaminB , productVitaminC , productVitaminD , productCalcium , productIron , productPotassium , productIngredients , productUnitCount , productProduceSoldAs , productVariety , productNumberofItems
+productGlutenFree , productFreshnessGuaranteed , productDietType , productUSDAOrganic , productKosher , productDietaryFibre , productSugars , productFlavourName , productSpecialty , productAllergenInfo , productAgeRange , productSpecificUsesForItem
+productSpecialIngredients , productServingsPerContainer , productTypicalPrice , productListPrice , productCuisine , productJoinProLabelonPage , productBiologicalSource , productTempretureSource , productYouSaveLabel , productPickupFrom , productCategory , 
+// NEW PARAMERTERS // 
+produuctisUnavailable , productCannotBeShippedToUser , productGetUpto100ToYourAccountLabel
+productisAriaplusTopChoice , productRatings , productSave20PercentWithTradeIn , productIsTheLatestModel , productWorksWithAi , productFreeInternationalShipping , productFreeUSShipping , productisAvailableAtALowerPriceFromOtherSellers
+productThereIsAnotherVersionOfThisProduct , productPurchasesThisMonth , productWarrantyandSupport , productNote , product1YearWarrantyand30DayReturnsPolicy , productCustomersUsuallyKeepThisItem , productLimitedTimeOffer , producthasAriaplusProtectionPlan , 
+         
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// PRODUCT DETAILS // 
+productColor: string | null;
+productSize: string | null;
+productSubscription: string | null;
+productConfiguration: string | null;
+productWeWantYouToKnow: string | null;
+productBrand: string | null;
+productDimensions: string | null;
+productMaterials: string | null;
+productConnectorType: string | null;
+productAboutThisItem: string | null;
+productMetalType: string | null;
+productClaspType: string | null;
+productChainType: string | null;
+productGenType: string | null;
+productItemType: string | null;
+productItemDisplayLenght: string | null;
+productItemWidth: string | null;
+productRecommendedUses: string | null;
+productOccassion : string | null;
+productCapacity: string | null;
+productItemWight: string | null;
+productASIN: string | null;
+productItemModelNumber: string | null;
+productBestSellersRank: string | null;
+productDateFirstAvailable: string | null;
+productManufacturer: string | null;
+productWhatsInTheBox: string | null;
+ProductDescription: string | null;
+productConectivityTechnology: string | null;
+productPrintingTechnology: string | null;
+productSpecialFeature: string | null;
+productModelName: string | null;
+productPrinterOutput: string | null;
+productMaximumPrintSpeed: string | null;
+productCompatibleMountings: string | null;
+productAspectRatio: string | null;
+productPhotoSensorTechnology: string | null;
+productSupportedFileFormat: string | null;
+productImageStabalization: string | null;
+productMaximumFocalLenght: string | null;
+productOpticalZoom: string | null;
+productMaximumAperture: string | null;
+productMaximumExpandedISO: string | null;
+productDeliveryandSupport: string | null;
+productInkColor: string | null;
+productPageYeild: string | null;
+productIncludedComponents: string | null;
+productScreenSize: string | null;
+productResolution: string | null;
+productScreenSurfaceDescription: string | null;
+productFrequencyBandClass: string | null;
+productWirelessCommunicationStandard: string | null;
+productFrequency: string | null;
+productOperatingSystem: string | null;
+productCPUModel: string | null;
+productCPUSpeed: string | null;
+productGraphicsCardDescription: string | null;
+productMemoryStorageCapacity: string | null;
+productMemorySlotsAvailable: string | null;
+productRamMemoryInstalledSize: string | null;
+productMountingType: string | null;
+productUPC: string | null;
+productOperatingMode: string | null;
+productCurrentRating: string | null;
+productOperatingVoltage: string | null;
+productTerminal: string | null;
+productSwitchType: string | null;
+productSpecificationMet: string | null;
+productCircuitType: string | null;
+productActuatorType: string | null;
+productCondition: string | null;
+productBatteryLife: string | null;
+productHeight: string | null;
+productWeight: string | null;
+productWidth: string | null;
+productDepth: string | null;
+productMountings: string | null;
+productChip: string | null;
+productVideo: string | null;
+productDisplay: string | null;
+productRatings: string | null;
+productCompatibility: string | null;
+productBatteries: string | null;
+productEarPlacement: string | null;
+productFormFactor: string | null;
+productNoiseControl: string | null;
+productHardDiskSize: string | null;
+productGPU: string | null;
+productNetworking: string | null;
+productInTheBox: string | null;
+productDesignName: string | null;
+productShape: string | null;
+productOS: string | null;
+productFabricType: string | null;
+productCareInstructions: string | null;
+productRingSize: string | null;
+productGenTYpe: string | null;
+productEditon: string | null;
+productPltaform: string | null;
+productSurroundSound: string | null;
+productAudio: string | null;
+productServiingSize: string | null;
+productCalories: string | null;
+productTotalFat: string | null;
+productStauratedFat: string | null;
+productTransFat: string | null;
+productColestrol: string | null;
+productSodium: string | null;
+productProtein: string | null;
+productVitaminA: string | null;
+productVitaminB: string | null;
+productVitaminC: string | null;
+productVitaminD: string | null;
+productCalcium: string | null;
+productIron: string | null;
+productPotassium: string | null;
+productIngredients: string | null;
+productUnitCount: string | null;
+productProduceSoldAs: string | null;
+productVariety: string | null;
+productNumberofItems: string | null;
+productGlutenFree: string | null;
+productFreshnessGuaranteed: string | null;
+productDietType: string | null;
+productUSDAOrganic: string | null;
+productKosher: string | null;
+productDietaryFibre: string | null;
+productSugars: string | null;
+productFlavourName: string | null;
+productSpecialty: string | null;
+productAllergenInfo: string | null;
+productAgeRange: string | null;
+productSpecificUsesForItem: string | null;
+productSpecialIngredients: string | null;
+productServingsPerContainer: string | null;
+productTypicalPrice: string | null;
+productListPrice: string | null;
+productCuisine: string | null;
+productJoinProLabelonPage: boolean | null ; 
+productBiologicalSource: string | null;
+productTempretureSource: string | null;
+productYouSaveLabel: boolean | null ; 
+productPickupFrom: string | null;
+productCategory: string | null;
+
+
+// NEW PARAMERTERS // 
+produuctisUnavailable: boolean | null ; 
+productCannotBeShippedToUser: boolean | null ; 
+productGetUpto100ToYourAccountLabel: boolean | null ; 
+productisAriaplusTopChoice: boolean | null ; 
+productSave20PercentWithTradeIn: boolean | null ; 
+productIsTheLatestModel: boolean | null ; 
+productWorksWithAi: boolean | null ; 
+productFreeInternationalShipping: boolean | null ; 
+productFreeUSShipping: boolean | null ; 
+productisAvailableAtALowerPriceFromOtherSellers: boolean | null ; 
+productThereIsAnotherVersionOfThisProduct: boolean | null ; 
+productPurchasesThisMonth: string | null;
+productWarrantyandSupport: string | null;
+productNote: string | null;
+product1YearWarrantyand30DayReturnsPolicy: boolean | null ; 
+productCustomersUsuallyKeepThisItem: boolean | null ; 
+productLimitedTimeOffer: boolean | null ; 
+producthasAriaplusProtectionPlan: boolean | null ; 
+
+
+
 
 
 
