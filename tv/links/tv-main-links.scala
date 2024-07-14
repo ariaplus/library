b4a0aff9a16873fdc08e248a20fada0b308594df
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-07-14 15:21:33 +1200

    Create tv-main-links.scala

---
+++ tv/links/tv-main-links.scala
@@ -0,0 +1,6 @@
+object - def - NAN
+/watching-now
+/tittle - def - for movies - and would replace the details - for movie and tv-show details 
+/staff-picks - def - for movies and other top picks from staff
+/episodes/ID-OF-MOVIE - for viewing episodes of tv-shows 
+/user / for actors and cast 
