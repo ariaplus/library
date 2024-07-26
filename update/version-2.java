b2772c1878484e8ed5ff6bbada143bf8a4659c75
Aria+ <117418742+ariaplus@users.noreply.github.com>
2024-07-26 16:20:28 +1200

    Create version-2.java

---
+++ update/version-2.java
@@ -0,0 +1,18 @@
+Version 2 UPDATE for +
+  Database values updates 
+  var - for - 'show'  - '/show'
+              'video store' - '/video-store'
+              'music' - '/music'
+              'play-center' - '/play-center'
+              'communities' - '/communities'
+              'inbox' - '/user/inbox'
+              'books' - '/book-store' - for learn
+              'articles' - '/artlces' - for News 
+              '
+
+  @lib/types
+  @lib/firebase/collections
+  firestore.rules
+  @lib/firebase/utils
+  @firebase.indexes.json
+  @
